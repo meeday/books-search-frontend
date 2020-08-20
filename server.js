f84d439d05095b03e310d16d0851120e7da760d1
meeday <59993824+meeday@users.noreply.github.com>
2020-08-20 15:43:42 +0100

    mongoose connection added

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 
@@ -8,15 +8,20 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 app.use(routes);
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooksearch",
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  }
+);
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-})
-
+});
