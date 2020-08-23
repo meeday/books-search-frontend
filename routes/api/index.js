7bcb4b42a68b63e726e886cb5391f722cdad7da8
meeday <59993824+meeday@users.noreply.github.com>
2020-08-23 16:09:11 +0100

    route paths specified

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const { Router } = require("express");
+const bookRoutes = require("./bookRoute");
+const googleRoutes = require("./googleRoute");
+
+Router.use("/books", bookRoutes);
+Router.use("/google", googleRoutes);
+
+Router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = Router;
