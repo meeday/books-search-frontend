f54a48f80931577a8cc9b3f5dad599de5f5e6644
meeday <59993824+meeday@users.noreply.github.com>
2020-08-23 16:12:29 +0100

    syntax edited

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,8 +6,8 @@ const googleRoutes = require("./googleRoute");
 Router.use("/books", bookRoutes);
 Router.use("/google", googleRoutes);
 
-Router.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
-});
+Router.use((req, res) => 
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"))
+);
 
 module.exports = Router;
