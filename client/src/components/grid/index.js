1f9c053fb673fa2231d703503d61c0bb31173ae0
meeday <59993824+meeday@users.noreply.github.com>
2020-08-23 17:49:55 +0100

    grid components updated to es6

--- client/src/components/grid/index.js
+++ client/src/components/grid/index.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-export function Container({ fluid, children }) {
-  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
-}
+export const Container = ({ fluid, children }) => {
+  <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+};
 
-export function Row({ fluid, children }) {
-  return <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
-}
+export const Row = ({ fluid, children }) => {
+  <div className={`row${fluid ? "-fluid" : ""}`}>{children}</div>;
+};
 
-export function Col({ size, children }) {
-  return (
-    <div
-      className={size
-        .split("")
-        .map((size) => `col-${size}`)
-        .join(" ")}
-    >
-      {children}
-    </div>
-  );
-}
+export const Col = ({ size, children }) => {
+  <div
+    className={size
+      .split("")
+      .map((size) => `col-${size}`)
+      .join(" ")}
+  >
+    {children}
+  </div>;
+};
