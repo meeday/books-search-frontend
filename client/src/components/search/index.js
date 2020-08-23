c2f9b9c45b34fe578adffe28d62c7576879e8986
meeday <59993824+meeday@users.noreply.github.com>
2020-08-23 18:08:49 +0100

    search bar component

---
+++ client/src/components/search/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function Search({ query, handleInputChange, handleSubmit }) {
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="Query">
+          <strong>Search Book</strong>
+        </label>
+        <input
+          className="form-control"
+          id="Title"
+          type="text"
+          value={query}
+          placeholder="search for book"
+          name="query"
+          onChange={handleInputChange}
+          required
+        />
+      </div>
+      <div className="pull-right">
+        <button onClick={handleSubmit} type="submit" className="btn btn-lg btn-primary float-right">
+          Search
+        </button>
+      </div>
+    </form>
+  );
+}
