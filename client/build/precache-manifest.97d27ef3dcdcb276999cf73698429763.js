ed9695b0c3b13ef1249295d0a898baec300247fd
meeday <59993824+meeday@users.noreply.github.com>
2020-08-24 18:37:31 +0100

    build run

---
+++ client/build/precache-manifest.97d27ef3dcdcb276999cf73698429763.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e2ff0e587283a695a53442f0937327",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617976917a4b9e474c66",
+    "url": "/static/css/main.fdff9d99.chunk.css"
+  },
+  {
+    "revision": "21b422bd5982774c2819",
+    "url": "/static/js/2.ddd8a4e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ddd8a4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617976917a4b9e474c66",
+    "url": "/static/js/main.65a018ed.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
