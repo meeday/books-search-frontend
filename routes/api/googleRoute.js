832e7a00c3e90fffd48d20f64815ed01e23ef843
meeday <59993824+meeday@users.noreply.github.com>
2020-08-24 15:01:35 +0100

    files removed

--- routes/api/googleRoute.js
+++
@@ -1,8 +0,0 @@
-const Rpouter = require("express").Router();
-const axiosController = require("../../controllers/axios");
-const { Router } = require("express");
-
-Router.route("/")
-.get(axiosController.findAll);
-
-module.exports= router;
