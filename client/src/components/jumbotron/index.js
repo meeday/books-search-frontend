bce7f618334d5e0a29586d9411efbfb347d70e44
meeday <59993824+meeday@users.noreply.github.com>
2020-08-24 15:00:20 +0100

    background image added

--- client/src/components/jumbotron/index.js
+++ client/src/components/jumbotron/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
 
-export default function Jumbotron({ children }) {
+function Jumbotron({ children }) {
   return <div className="jumbotron mt-4">{children}</div>;
 }
+
+export default Jumbotron;
