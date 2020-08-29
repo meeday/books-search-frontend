e0700f7bf3b08a7a6ef748871586409225bfc96f
meeday <59993824+meeday@users.noreply.github.com>
2020-08-29 11:13:41 +0100

    api routes edited

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,24 +3,24 @@ import axios from "axios";
 export default {
   // Gets books from the Google API
   getBooks: async (q) => {
-    const result = await axios.get("/api/google", {
+    const result = await axios.get("https://meeday-books-app.herokuapp.com/api/google", {
       params: { q: "title:" + q },
     });
     return result;
   },
   // Gets all saved books
   getSavedBooks: async () => {
-    const result = await axios.get("/api/books");
+    const result = await axios.get("https://meeday-books-app.herokuapp.com/api/books");
     return result;
   },
   // Deletes the saved book with the given id
   deleteBook: async (id) => {
-    const result = await axios.delete("/api/books/" + id);
+    const result = await axios.delete("https://meeday-books-app.herokuapp.com/api/books/" + id);
     return result;
   },
   // Saves an book to the database
   saveBook: async (bookData) => {
-    const result = await axios.post("/api/books", bookData);
+    const result = await axios.post("https://meeday-books-app.herokuapp.com/api/books", bookData);
     return result;
   },
 };
